f58c7fe505543efc10663259204e0cbd31fee54c
Carl Thomé <carlthome@gmail.com>
2012-04-15 22:04:32 +0200

    Cleaned up various resources.

--- src/kth/csc/inda/pockettheremin/PocketThereminActivity.java
+++ src/kth/csc/inda/pockettheremin/PocketThereminActivity.java
@@ -26,30 +26,26 @@ import android.widget.Toast;
  * The theremin will reach higher frequencies if there's more light in the room.
  * Turn on the lights!
  * 
- * @author Carl
- * 
  */
 public class PocketThereminActivity extends Activity implements
 		SensorEventListener {
 	/*
-	 * // TODO Find out why Xperia phones don't seem to provide a light sensor
+	 * TODO Find out why Xperia phones don't seem to provide a light sensor
 	 * through the SensorManager. For now the accelerometer is used instead.
 	 * Fun!
 	 */
-	
+
 	/*
-	 * TODO Emulate the sound of a theremin (instead of a pure sinus curve).
+	 * TODO Emulate the sound of a theremin (instead of a pure sine curve).
 	 */
-	
+
 	/*
 	 * TODO Prepare a preference menu.
 	 */
 
 	static final boolean DEBUG = true;
-
-	private SensorManager mSensorManager;
-	private Sensor mLight, mProximity, mAccelerometer;
-
+	private SensorManager sensors;
+	private Sensor light, proximity, accelerometer;
 	private AsyncTask soundGenerator;
 	private AudioTrack sound;
 	private float pitch, amplitude;
@@ -65,30 +61,25 @@ public class PocketThereminActivity extends Activity implements
 		setContentView(R.layout.main);
 
 		// Find sensors with the SensorManager.
-		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-		mAccelerometer = mSensorManager
-				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+		sensors = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		light = sensors.getDefaultSensor(Sensor.TYPE_LIGHT);
+		accelerometer = sensors.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		proximity = sensors.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
 		if (DEBUG) {
-			pitch = 1000;
-			amplitude = 100;
-			minFrequency = 200;
-			maxFrequency = 10000;
+			amplitude = 100; // TODO Should be set by a sensor.
+			minFrequency = 200; // TODO Should be an user preference.
+			maxFrequency = 6000; // TODO Should be an user preference.
 		}
 
 		if (DEBUG)
-			for (Sensor sensor : mSensorManager.getSensorList(Sensor.TYPE_ALL))
+			for (Sensor sensor : sensors.getSensorList(Sensor.TYPE_ALL))
 				Log.e("Sensors", sensor.getName());
 	}
 
 	// TODO What is the use of this method?
 	@Override
 	public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-		if (DEBUG)
-			alert(Integer.toString(accuracy));
-
 		Log.i(sensor.getName(), "onAccuracyChanged: " + accuracy);
 	}
 
@@ -105,16 +96,14 @@ public class PocketThereminActivity extends Activity implements
 	@Override
 	public final void onSensorChanged(SensorEvent event) {
 		Sensor sensor = event.sensor;
-		
-		if (sensor.getType() == Sensor.TYPE_LIGHT
-				|| sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+		if (sensor.getType() == Sensor.TYPE_LIGHT || sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
 			float step = (maxFrequency - minFrequency) / sensor.getResolution();
 			pitch = event.values[0] * step;
 		} else if (sensor.getType() == Sensor.TYPE_PROXIMITY) {
 			if (play) {
 				play = false;
 				alert("Stop.");
-				soundGenerator.cancel(true);
 			} else {
 				play = true;
 				soundGenerator = new SoundGenerator().execute();
@@ -131,13 +120,12 @@ public class PocketThereminActivity extends Activity implements
 	@Override
 	protected void onResume() {
 		super.onResume();
-		mSensorManager.registerListener(this, mProximity,
+		sensors.registerListener(this, proximity,
 				SensorManager.SENSOR_DELAY_FASTEST);
-		mSensorManager.registerListener(this, mAccelerometer,
-				SensorManager.SENSOR_DELAY_GAME);
-		mSensorManager.registerListener(this, mLight,
-				SensorManager.SENSOR_DELAY_GAME);
-		
+		sensors.registerListener(this, accelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		sensors.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
+
 		alert("Ready...");
 	}
 
@@ -148,13 +136,17 @@ public class PocketThereminActivity extends Activity implements
 	@Override
 	protected void onPause() {
 		super.onPause();
-		mSensorManager.unregisterListener(this);
+		sensors.unregisterListener(this);
 
 		if (sound.getState() == AudioTrack.STATE_INITIALIZED)
 			sound.release();
 	}
 
-	private class SoundGenerator extends AsyncTask<Void, Void, Void> {
+	/**
+	 * Generate sounds in a separate thread (as an AsyncTask) by sampling a
+	 * frequency. The frequency is modulated by the sensor provided pitch.
+	 */
+	private class SoundGenerator extends AsyncTask<Void, Float, String> {
 		float frequency, increment, angle;
 		short samples[] = new short[1024];
 		int sampleRate = 22050;
@@ -170,32 +162,39 @@ public class PocketThereminActivity extends Activity implements
 			sound.play();
 		}
 
-		protected Void doInBackground(Void... params) {
+		protected String doInBackground(Void... params) {
 			while (PocketThereminActivity.this.play) {
 				for (int i = 0; i < samples.length; i++) {
-					
+
 					frequency = PocketThereminActivity.this.pitch + minFrequency;
-					
+
 					if (frequency > maxFrequency)
 						frequency = maxFrequency;
-					
+
 					increment = (float) (2 * Math.PI * frequency) / sampleRate;
 					samples[i] = (short) ((float) Math.sin(angle) * Short.MAX_VALUE);
 					angle += increment;
 				}
-
+				//publishProgress(frequency); No, keep it simple!
 				sound.write(samples, 0, samples.length);
 			}
-			return null;
+
+			return "Done.";
 		}
 
-		protected void onPostExecute(Void result) { // TODO Redundant method?
+		protected void onProgressUpdate(Float... progress) {
+			alert(Float.toString(progress[0]));
+		}
+
+		protected void onPostExecute(String result) { // TODO Redundant method?
+			alert(result);
+			sound.flush();
 			sound.release(); // TODO Flush first?
 		}
 	}
 
 	/**
-	 * Provide popups to the user.
+	 * Provide feedback to the user.
 	 */
 	private void alert(String s) {
 		Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
